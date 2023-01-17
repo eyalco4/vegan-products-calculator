7314fa2ed793a112e5fecaee95b76c2d62df12a0
eyalco4 <eyalc4444@gmail.com>
2023-01-17 20:52:26 +0200

    Phase 6  (#6)
    * adding some data
    
    * adding dots elastic, adding more products
    
    Co-authored-by: ecohen2 <eyal_cohen@intuit.com>

--- src/components/Totals.tsx
+++ src/components/Totals.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'src/components/Totals.css';
 interface Props {
   totalProtein: string;
@@ -7,20 +7,39 @@ interface Props {
 }
 
 function Totals({ totalProtein, totalCarbs, totalCalories }: Props) {
+  const [showLoadingEffect, setShowLoadingEffect] = useState(true);
+  useEffect(() => {
+    setShowLoadingEffect(true);
+    setTimeout(() => {
+      setShowLoadingEffect(false);
+    }, 2000);
+  }, [totalCalories]);
   return (
     <div className="fixed-bottom">
       <div className="totals-wrapper">
         <div className="totals protein">
           <div className="total-key">Total Protein</div>
-          <div className="total-value">{totalProtein}</div>
+          {showLoadingEffect ? (
+            <div className="dot-elastic"></div>
+          ) : (
+            <div className="total-value">{totalProtein}</div>
+          )}
         </div>
         <div className="totals carbs">
           <div className="total-key">Total Carbs</div>
-          <div className="total-value">{totalCarbs}</div>
+          {showLoadingEffect ? (
+            <div className="dot-elastic"></div>
+          ) : (
+            <div className="total-value">{totalCarbs}</div>
+          )}{' '}
         </div>
         <div className="totals calories">
           <div className="total-key">Total Calories</div>
-          <div className="total-value">{totalCalories}</div>
+          {showLoadingEffect ? (
+            <div className="dot-elastic"></div>
+          ) : (
+            <div className="total-value">{totalCalories}</div>
+          )}{' '}
         </div>
       </div>
     </div>
