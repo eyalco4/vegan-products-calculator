340ba5712b6538a925081ad35fbca085d88d9a38
ecohen2 <eyal_cohen@intuit.com>
2023-02-23 17:17:47 +0200

    SAVEPOINT

---
+++ src/stories/Totals.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Totals from 'src/components/Totals';
+
+const styles = {
+  transform: 'scale(1)',
+  height: '50vh',
+};
+export default {
+  title: 'components/Totals',
+  component: Totals,
+  decorators: [(storyFn) => <div style={styles}>{storyFn()}</div>],
+  argTypes: {
+    totalProtein: { control: 'text' },
+    totalCarbs: { control: 'text' },
+    totalCalories: { control: 'text' },
+  },
+} as ComponentMeta<typeof Totals>;
+
+const Template: ComponentStory<typeof Totals> = (args) => <Totals {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  totalProtein: '140',
+  totalCarbs: '860',
+  totalCalories: '2300',
+};
